50ac8ec4a52e06cbf829c84a77cc5d0ab525e5c8
Jamison-Jia <18842620631@163.com>
2026-04-13 14:14:09 +0800

    feat: CatalogApi - Get products By ID

---
+++ section5/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdResult(Product Product);
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public class GetProductByIdHandler (IDocumentSession session, ILogger<GetProductByIdHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, 
+        CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"{nameof(GetProductByIdHandler)}.Handle called with {query}");
+        
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+        
+        return new  GetProductByIdResult(product);
+    }
+}
