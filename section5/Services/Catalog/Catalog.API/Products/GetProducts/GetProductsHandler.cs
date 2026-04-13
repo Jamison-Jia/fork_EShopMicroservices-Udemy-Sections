16e089f4947c4d8167e421427e5289010ba56e50
Jamison-Jia <18842620631@163.com>
2026-04-13 13:38:33 +0800

    feat: CatalogApi - Get products func added

---
+++ section5/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,23 @@
+﻿using Marten.Linq;
+
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductQuery() : IQuery<GetProductResult>;
+public record GetProductResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession session,
+                                        ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductQuery, GetProductResult>
+{
+    public async Task<GetProductResult> Handle(GetProductQuery query,
+                                         CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"{nameof(GetProductsQueryHandler)}.Handle called with {query}");
+
+        var products =
+            await ((IMartenQueryable)session.Query<Product>())
+                                            .ToListAsync<Product>(cancellationToken);
+
+        return new GetProductResult(products);
+    }
+}
