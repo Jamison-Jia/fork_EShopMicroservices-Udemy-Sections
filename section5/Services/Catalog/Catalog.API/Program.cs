a36db5a80f4c7e2d2f0db4da8f6fc718d48d97ec
JJ <18842620631@163..com>
2026-04-24 14:57:02 +0800

    feat: CatalogApi - HealthCheck endpoint added

--- section5/Services/Catalog/Catalog.API/Program.cs
+++ section5/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,7 @@
 using BuildingBlocks.Behaviors;
 using Catalog.API.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,9 +27,18 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddValidatorsFromAssembly(assembly);
 
+builder.Services.AddHealthChecks()
+                .AddNpgSql(builder.Configuration.GetConnectionString("Database"));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
+
+app.UseHealthChecks("/health", new HealthCheckOptions()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
