fcb34202dccb1ea34127e8e0b4cd627dda2c5ccc
Jamison-Jia <18842620631@163.com>
2026-05-03 19:31:06 +0800

    feat: Ordering - DDD abstractions added

---
+++ section5/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions;
+public interface IDomainEvent : INotification
+{
+    Guid EventId => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    
+    /// <summary>
+    /// /Represents which class throwing this domain event understanding from the assembly
+    /// </summary>
+    public string EventType => GetType().AssemblyQualifiedName;
+}
