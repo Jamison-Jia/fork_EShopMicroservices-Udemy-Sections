d170b5c9073c168109eebc58e725d4d2c2844209
Jamison-Jia <18842620631@163.com>
2026-05-03 19:18:12 +0800

    feat: Ordering - CleanArch projs and refes added

---
+++ section5/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        // add carter
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        // app.usercarter();
+
+        return app;
+    }
+}
---
+++ section5/Services/Ordering/Ordering.Api/Program.cs
@@ -0,0 +1,18 @@
+using Ordering.Api;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+       .AddApplicationServices()
+       .AddInfrastructureServices(builder.Configuration)
+       .AddApiServices();
+
+var app = builder.Build();
+
+app.UseApiServices();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
