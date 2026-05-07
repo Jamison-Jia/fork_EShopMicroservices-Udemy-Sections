9361dcf1bf26437e100a6cfc45d2d2ec089adfb8
JJ <18842620631@163..com>
2026-05-07 19:37:08 +0800

    feat: Ordering - GetOrders By name or Customer

---
+++ section5/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,16 @@
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders
+            .Where(o => query.OrderName == o.OrderName.Value)
+            .ToListAsync();
+
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
+    }
+}
