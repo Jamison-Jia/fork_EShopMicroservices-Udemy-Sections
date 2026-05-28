423973a53f8877ae15766e0314c320262fffe9e7
JJ <18842620631@163..com>
2026-05-28 14:47:42 +0800

    feat: Ordering - exception handler for OrderNotFoundEx

--- section5/Services/Ordering/Ordering.Api/DependencyInjection.cs
+++ section5/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Carter;
+﻿using BuildingBlocks.Exceptions.Handler;
+using Carter;
 
 namespace Ordering.Api;
 
@@ -9,12 +10,16 @@ public static class DependencyInjection
         // add carter
         services.AddCarter();
 
+        services.AddExceptionHandler<CustomExceptionHandler>();
+
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
         app.MapCarter();
+
+        app.UseExceptionHandler(options => { });
 
         return app;
     }
