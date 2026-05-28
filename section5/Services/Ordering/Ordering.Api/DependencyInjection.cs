2fcf3547d3e7dedfd851287c23b958a0a7e835cc
JJ <18842620631@163..com>
2026-05-28 14:48:55 +0800

    feat: OrderingApi - health check added

--- section5/Services/Ordering/Ordering.Api/DependencyInjection.cs
+++ section5/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -1,16 +1,21 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
 using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.Api;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
         // add carter
         services.AddCarter();
 
         services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
 
         return services;
     }
@@ -20,6 +25,11 @@ public static class DependencyInjection
         app.MapCarter();
 
         app.UseExceptionHandler(options => { });
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions()
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
         return app;
     }
