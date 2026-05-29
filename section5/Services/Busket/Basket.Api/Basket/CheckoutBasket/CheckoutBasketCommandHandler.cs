ff041bd958ab35d982b88c3dd2e0f6f6d7cf9ab7
JJ <18842620631@163..com>
2026-05-29 16:13:58 +0800

    feat: RabbitMQ - Consume BasketCheckoutEvent in Ordering.Application

--- section5/Services/Busket/Basket.Api/Basket/CheckoutBasket/CheckoutBasketCommandHandler.cs
+++ section5/Services/Busket/Basket.Api/Basket/CheckoutBasket/CheckoutBasketCommandHandler.cs
@@ -22,7 +22,7 @@ public class CheckoutBasketCommandHandler(IBasketRepository repository,
             return new CheckoutBasketResult(false);
         }
 
-        var eventMessage = command.BasketCheckoutDto.Adapt<BasketCheckoutEvent>();
+        BasketCheckoutEvent eventMessage = command.BasketCheckoutDto.Adapt<BasketCheckoutEvent>();
         eventMessage.TotalPrice = basket.TotalPrice;
 
         await publishEndpoint.Publish(eventMessage, cancellationToken);
--- section5/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ section5/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,18 +1,25 @@
 ﻿using System.Reflection;
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddMediatR(cfg =>
         {
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
+
+        // Msg consumer should provide the assembly explicitly.
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
         
         return services;
     }
