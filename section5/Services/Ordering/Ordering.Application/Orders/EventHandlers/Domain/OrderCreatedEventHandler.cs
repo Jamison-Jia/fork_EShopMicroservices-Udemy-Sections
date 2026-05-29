f9e892fa87a5527996404f247dbcb1ecf8e1e518
JSoleraJ <18842620631@163.com>
2026-05-29 23:24:19 +0800

    feat: RabbitMQ - Feature management for OrdeFullfilment in OrderCreatedEventhandler

--- section5/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ section5/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,17 +1,19 @@
 ﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
-public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint, 
-                                      IFeatureManager featureManager, 
+
+public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint,
+                                      IFeatureManager featureManager,
                                       ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
-    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    public async Task Handle(OrderCreatedEvent domainEvent,
+                             CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
         if (await featureManager.IsEnabledAsync("OrderFullfilment"))
         {
-            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
-            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+            OrderDto orderDto = domainEvent.order.ToOrderDto();
+            await publishEndpoint.Publish(orderDto, cancellationToken);
         }
     }
 }
